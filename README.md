7cff9e121b86a31a87ea1acfd06f30a1b445678b
Jonas Birkelöf <github@jmail.nu>
2023-01-10 16:49:47 +0100

    First commit

---
+++ README.md
@@ -0,0 +1,38 @@
+# Beets Layout
+
+# About
+Beets Layout is a CSS and JavaScript library that gives you some good layout fundation. for your html or PHP projects. The focus is on dashboards but you can easily modify it to make it work with standard websites or single page applications.
+
+Beets Layout can be used by itself with some css styling, but if you want a super simple start you can use it with [Bootstrap](https://getbootstrap.com/) and [documentation](https://jonasbirkelof.github.io/beets-css)!
+
+# Download
+
+For the latest version of Beets CSS, plese see the [GitHub Releases](https://github.com/jonasbirkelof/beets-layout/releases) page.
+
+The files are located under the section **Assets** of the version you want.
+
+- Compiled CSS: `beets-css-v2.x.x-dist.zip`
+- Source files: `beets-css-v2.x.x-src.zip`
+
+# Installation
+
+## Compiled CSS files
+
+1. Download and unzip the compiled version of Beets Layout `beets-css-v2.x.x-dist.zip`.
+2. Include `beets-layout.css` and `beets-layout.js` in the `<head>` of your projects index file.
+```html
+<link rel="stylesheet" href="~/assets/css/beets-layout.css">
+<script src="~/assets/js/beets-layout.js"></script>
+```
+
+## Source files
+
+1. Download and unzip the Beets CSS source files `beets-css-v2.x.x-src.zip`.
+2. Move the folders `scss/beets-layout` and `js/beets-layout` to your resource folder in your project (from where you compile your resources, i.e. ~/resources/`).
+3. Compile `beets-layout.scss` and `betts-layout.js` into you public assets folder (i.e. ~/public/assets/).
+
+# Documentation
+
+Please visit the documentations site for instruction on how to customize and use Beets Layout.
+
+[Beets Layout Documentation](https://jonasbirkelof.github.io/beets-layout/latest)
---
+++ index.php
@@ -0,0 +1,152 @@
+<?php
+
+function pageIsActive($uris) 
+{
+	$pageIsActive = false;
+	$page = isset($_GET['page']) ? $_GET['page'] : null;
+
+	foreach ($uris as $uri) {
+		if ($page == $uri) $pageIsActive = true;
+	}
+
+	return $pageIsActive;
+}
+
+function activePage($uris) 
+{
+	return pageIsActive($uris) ? 'active' : null;
+}
+
+function openSubmenu($uris) 
+{
+	return pageIsActive($uris) ? 'open' : null;
+}
+
+?>
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Beets Layout</title>
+	<!-- Bootstrap -->
+    <link rel="stylesheet" href="./dist/css/bootstrap.css">
+    <script src="./dist/js/bootstrap.bundle.js"></script>
+	<!-- Beets layout -->
+    <link rel="stylesheet" href="./dist/css/beets-layout.css">
+    <script src="./dist/js/beets-layout.js"></script>
+</head>
+<body>
+
+    <!-- Beets CSS layout wrapper -->
+	<div id="beets-layout">
+		
+		<!-- Sidebar -->
+		<aside class="bl__sidebar sidebar-collapse sidebar-dark" style="background-color: #045163;">
+
+			<div class="sidebar__header justify-content-between">
+				<div class="header__logo">
+					<img src="" alt="Logotype">
+				</div>
+				<button class="btn btn-primary d-lg-none" onclick="toggleSidebar()">X</button>
+			</div>
+
+			<div class="sidebar__body">
+				<!-- Navigation -->
+				<nav class="sidebar__nav nav-rounded nav-compact-- nav-accent-warning">
+					<ul class="nav-list">
+						<li class="list-item">
+							<a href="?page=dashboard" class="nav-link <?= activePage(['dashboard']) ?>">
+								<div class="nav-link-icon">&lt;i&gt;</div>
+								Dashboard
+							</a>
+						</li>
+						<?php $submenuItems = ['addUser', 'listUsers', 'removeUser']; ?>
+						<li class="list-item <?= openSubmenu($submenuItems) ?>">
+							<a href="#" class="nav-link nav-link__submenu <?= activePage($submenuItems) ?>" id="sub_users" onclick="toggleSubmenu('sub_users')">
+								<div class="nav-link-icon">&lt;i&gt;</div>
+								Users
+							</a>
+							<ul class="nav-list__submenu">
+								<li class="list-item"><a href="?page=addUser" class="nav-link <?= activePage(['addUser']) ?>">Add user</a></li>
+								<li class="list-item"><a href="?page=listUsers" class="nav-link <?= activePage(['listUsers']) ?>">List users</a></li>
+								<li class="list-item"><a href="?page=removeUser" class="nav-link <?= activePage(['removeUser']) ?>">Remove user</a></li>
+							</ul>
+						</li>
+						<li class="list-item">
+							<a href="?page=settings" class="nav-link <?= activePage(['settings']) ?>">
+								<div class="nav-link-icon">&lt;i&gt;</div>
+								Settings
+							</a>
+						</li>
+					</ul>
+				</nav>
+
+				<hr class="sidebar__divider">
+
+				<nav class="sidebar__nav nav-compact nav-accent">
+					<ul class="nav-list">
+						<li class="list-item">
+							<a href="?page=link-1" class="nav-link <?= activePage(['link-1']) ?>">Link 1</a>
+						</li>
+						<li class="list-item">
+							<a href="?page=link-2" class="nav-link <?= activePage(['link-2']) ?>">Link 2</a>
+						</li>
+						<li class="list-item">
+							<a href="?page=link-3" class="nav-link <?= activePage(['link-3']) ?>">Link 3</a>
+						</li>
+					</ul>
+				</nav>
+
+			</div>
+
+			<div class="sidebar__footer">
+				SIDEBAR FOOTER
+			</div>
+
+		</aside>
+			
+		<!-- Body -->
+		<div class="bl__body">
+
+			<!-- Header -->
+			<header class="bl__header justify-content-between header-sticky">
+				
+				<button class="btn btn-primary d-lg-none" onclick="toggleSidebar()">=</button>
+				<div class="header__logo d-lg-none">
+					<img src="" alt="Logotype">
+				</div>
+				<button class="btn btn-primary d-lg-none" onclick="toggleHeader('header-nav')">...</button>
+
+				<div class="header-collapse" id="header-nav">
+					<nav class="navbar__nav">
+						<ul class="nav-list">
+							<li class="nav-item"><a href="?page=home" class="nav-link <?= activePage(['home']) ?>">Home</a></li>
+							<li class="nav-item"><a href="?page=about" class="nav-link <?= activePage(['about']) ?>">About Us</a></li>
+							<li class="nav-item"><a href="?page=contact" class="nav-link <?= activePage(['contact']) ?>">Contact</a></li>
+						</ul>
+					</nav>
+				</div>
+
+			</header>
+
+			<!-- Main -->
+			<div class="bl__main">
+				<div class="container-xl px-2 px-lg-4">
+					MAIN
+				</div>
+			</div>		
+			
+			<!-- Footer -->
+			<div class="bl__footer">
+				FOOTER
+			</div>
+
+		</div>
+
+	</div>
+
+</body>
+</html>
